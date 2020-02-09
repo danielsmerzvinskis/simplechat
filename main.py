c89be3b383fc203562f3660b69a1d3cfedc97928
danielsmerzvinskis <rizikslvv@gmail.com>
2020-02-09 20:20:41 +0200

    hz kas pa hujnu

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template("home.html")
+
+@app.route('/chat')
+def chat():
+  return render_template("chat.html")
+
+@app.route('/chat2')
+def chat2():
+  return render_template("chat2.html")  
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port = 5420, threaded = True, debug = True)
